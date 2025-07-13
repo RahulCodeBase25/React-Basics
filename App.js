10e8ad1d0e0244503be05899e0d2bb0da5a8611c
Rahul Singh <rahul.codecraft25@Gmail.com>
2025-07-13 13:10:57 +0530

    From react to reacr-dom installed and parcel build also ✅

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am Rahul Singh!!"),
-    React.createElement("h2", {}, "I am Learning React!!"),
+    React.createElement("h2", {}, "React learning has been started again"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am Honey Singh!!"),
